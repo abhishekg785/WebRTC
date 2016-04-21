4a3867e786d97ad747cd3612285fe7989e0c4931
ABHISHEK GSWAMI <abhishekg785@gmail.com>
2016-04-21 13:34:44 +0530

    project added

---
+++ video&audioChat/server.js
@@ -0,0 +1,7 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)();
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(2013);
+console.log('Listening on port 2013');
